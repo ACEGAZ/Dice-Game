6dd393609fb4de83dfb85ab0c61b0c1ee7c4a2ec
ACEGAZ <garylowles@gmail.com>
2022-03-31 20:40:26 +0000

    add javascript function to show d6 game and hide start menu

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//player and computer scores//
+let playerScore = 1;
+let computerScore = 1;
+
+
+//6 sided dice game//
+function showhideD6Game () {
+    document.getElementById('d6-game').style.display = 'block';
+    document.getElementById('start-menu').style.display = 'none';
+} 
