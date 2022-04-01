8abdefcd07f2e86dd0b70761b38195ef4a318642
ACEGAZ <garylowles@gmail.com>
2022-04-01 21:30:23 +0000

    add alt text to all dice images

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ function playD6Game(){
       
 //d6 images//    
     let d6DiceImageArray = [
-                          d6img1 = 'assets/images/d6(1).png', 
+                          d6img1 = 'assets/images/d6(1).png',
                           d6img2 = 'assets/images/d6(2).png', 
                           d6img3 = 'assets/images/d6(3).png', 
                           d6img4 = 'assets/images/d6(4).png',
@@ -28,30 +28,42 @@ function playD6Game(){
 
 //display d6 images to player-result div//
      if (d6PlayerRoll === 1) {
+    document.getElementById('d6-dice-image-one').alt = "you rolled a 1";
     document.getElementById('d6-dice-image-one').src = d6DiceImageArray[0];
    } if (d6PlayerRoll === 2) {
+    document.getElementById('d6-dice-image-one').alt = "you rolled a 2";
     document.getElementById('d6-dice-image-one').src = d6DiceImageArray[1];
    } if (d6PlayerRoll === 3) {
+    document.getElementById('d6-dice-image-one').alt = "you rolled a 3";
     document.getElementById('d6-dice-image-one').src = d6DiceImageArray[2];
    } if (d6PlayerRoll === 4) {
+    document.getElementById('d6-dice-image-one').alt = "you rolled a 4";
     document.getElementById('d6-dice-image-one').src = d6DiceImageArray[3];
    } if (d6PlayerRoll === 5) {
+    document.getElementById('d6-dice-image-one').alt = "you rolled a 5";
     document.getElementById('d6-dice-image-one').src = d6DiceImageArray[4];
    } if (d6PlayerRoll === 6) {
+    document.getElementById('d6-dice-image-one').alt = "you rolled a 6";
     document.getElementById('d6-dice-image-one').src = d6DiceImageArray[5];
 
   //display d6 images to computer-result div//  
    } if (d6ComputerRoll === 1) {
+    document.getElementById('d6-dice-image-two').alt = "you rolled a 1";
     document.getElementById('d6-dice-image-two').src = d6DiceImageArray[0];
    } if (d6ComputerRoll === 2) {
+    document.getElementById('d6-dice-image-two').alt = "you rolled a 2";
     document.getElementById('d6-dice-image-two').src = d6DiceImageArray[1];
    } if (d6ComputerRoll === 3) {
+    document.getElementById('d6-dice-image-two').alt = "you rolled a 3";
     document.getElementById('d6-dice-image-two').src = d6DiceImageArray[2];
    } if (d6ComputerRoll === 4) {
+    document.getElementById('d6-dice-image-two').alt = "you rolled a 4";
     document.getElementById('d6-dice-image-two').src = d6DiceImageArray[3];
    } if (d6ComputerRoll === 5) {
+    document.getElementById('d6-dice-image-two').alt = "you rolled a 5";
     document.getElementById('d6-dice-image-two').src = d6DiceImageArray[4];
    } if (d6ComputerRoll === 6) {
+    document.getElementById('d6-dice-image-two').alt = "you rolled a 6";
     document.getElementById('d6-dice-image-two').src = d6DiceImageArray[5];
 }
    
@@ -113,47 +125,67 @@ function showhideD10Game() {
 
 //display d10 images to player-result div//
      if (d10PlayerRoll === 1) {
+    document.getElementById('d10-dice-image-one').alt = "you rolled a 1";
     document.getElementById('d10-dice-image-one').src = d10DiceImageArray[0];
    } if (d10PlayerRoll === 2) {
+    document.getElementById('d10-dice-image-one').alt = "you rolled a 2";
     document.getElementById('d10-dice-image-one').src = d10DiceImageArray[1];
    } if (d10PlayerRoll === 3) {
+    document.getElementById('d10-dice-image-one').alt = "you rolled a 3";
     document.getElementById('d10-dice-image-one').src = d10DiceImageArray[2];
    } if (d10PlayerRoll === 4) {
+    document.getElementById('d10-dice-image-one').alt = "you rolled a 4";
     document.getElementById('d10-dice-image-one').src = d10DiceImageArray[3];
    } if (d10PlayerRoll === 5) {
+    document.getElementById('d10-dice-image-one').alt = "you rolled a 5";
     document.getElementById('d10-dice-image-one').src = d10DiceImageArray[4];
    } if (d10PlayerRoll === 6) {
+    document.getElementById('d10-dice-image-one').alt = "you rolled a 6";
     document.getElementById('d10-dice-image-one').src = d10DiceImageArray[5];
    } if (d10PlayerRoll === 7) {
+    document.getElementById('d10-dice-image-one').alt = "you rolled a 7";
     document.getElementById('d10-dice-image-one').src = d10DiceImageArray[6];
    } if (d10PlayerRoll === 8) {
+    document.getElementById('d10-dice-image-one').alt = "you rolled a 8";
     document.getElementById('d10-dice-image-one').src = d10DiceImageArray[7];
    } if (d10PlayerRoll === 9) {
+    document.getElementById('d10-dice-image-one').alt = "you rolled a 9";
     document.getElementById('d10-dice-image-one').src = d10DiceImageArray[8];
    } if (d10PlayerRoll === 10) {
+    document.getElementById('d10-dice-image-one').alt = "you rolled a 10";
     document.getElementById('d10-dice-image-one').src = d10DiceImageArray[9];
    
 
 //display d10 images to computer-result div//
    } if (d10ComputerRoll === 1) {
+    document.getElementById('d10-dice-image-two').alt = "you rolled a 1";
     document.getElementById('d10-dice-image-two').src = d10DiceImageArray[0];
    } if (d10ComputerRoll === 2) {
+    document.getElementById('d10-dice-image-two').alt = "you rolled a 2";
     document.getElementById('d10-dice-image-two').src = d10DiceImageArray[1];
    } if (d10ComputerRoll === 3) {
+    document.getElementById('d10-dice-image-two').alt = "you rolled a 3";
     document.getElementById('d10-dice-image-two').src = d10DiceImageArray[2];
    } if (d10ComputerRoll === 4) {
+    document.getElementById('d10-dice-image-two').alt = "you rolled a 4";
     document.getElementById('d10-dice-image-two').src = d10DiceImageArray[3];
    } if (d10ComputerRoll === 5) {
+    document.getElementById('d10-dice-image-two').alt = "you rolled a 5";
     document.getElementById('d10-dice-image-two').src = d10DiceImageArray[4];
    } if (d10ComputerRoll === 6) {
+    document.getElementById('d10-dice-image-two').alt = "you rolled a 6";
     document.getElementById('d10-dice-image-two').src = d10DiceImageArray[5];
    } if (d10ComputerRoll === 7) {
+    document.getElementById('d10-dice-image-two').alt = "you rolled a 7";
     document.getElementById('d10-dice-image-two').src = d10DiceImageArray[6];
    } if (d10ComputerRoll === 8) {
+    document.getElementById('d10-dice-image-two').alt = "you rolled a 8";
     document.getElementById('d10-dice-image-two').src = d10DiceImageArray[7];
    } if (d10ComputerRoll === 9) {
+    document.getElementById('d10-dice-image-two').alt = "you rolled a 9";
     document.getElementById('d10-dice-image-two').src = d10DiceImageArray[8];
    }if (d10ComputerRoll === 10) {
+    document.getElementById('d10-dice-image-two').alt = "you rolled a 10";
     document.getElementById('d10-dice-image-two').src = d10DiceImageArray[9];
    }
   
@@ -225,87 +257,127 @@ function showhideD20Game() {
 
 //display d20 images to player-result div//
      if (d20PlayerRoll === 1) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 1";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[0];
    } if (d20PlayerRoll === 2) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 2";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[1];
    } if (d20PlayerRoll === 3) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 3";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[2];
    } if (d20PlayerRoll === 4) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 4";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[3];
    } if (d20PlayerRoll === 5) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 5";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[4];
    } if (d20PlayerRoll === 6) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 6";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[5];
    } if (d20PlayerRoll === 7) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 7";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[6];
    } if (d20PlayerRoll === 8) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 8";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[7];
    } if (d20PlayerRoll === 9) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 9";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[8];
    } if (d20PlayerRoll === 10) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 10";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[9];
    } if (d20PlayerRoll === 11) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 11";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[10];
    } if (d20PlayerRoll === 12) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 12";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[11];
    } if (d20PlayerRoll === 13) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 13";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[12];
    } if (d20PlayerRoll === 14) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 14";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[13];
    } if (d20PlayerRoll === 15) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 15";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[14];
    } if (d20PlayerRoll === 16) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 16";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[15];
    } if (d20PlayerRoll === 17) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 17";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[16];
    } if (d20PlayerRoll === 18) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 18";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[17];
    } if (d20PlayerRoll === 19) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 19";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[18];
    }if (d20PlayerRoll === 20) {
+    document.getElementById('d20-dice-image-one').alt = "you rolled a 20";
     document.getElementById('d20-dice-image-one').src = d20DiceImageArray[19];
    } 
 
   //display d20 images to computer-result div// 
      if (d20ComputerRoll === 1) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 1";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[0];
    } if (d20ComputerRoll === 2) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 2";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[1];
    } if (d20ComputerRoll === 3) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 3";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[2];
    } if (d20ComputerRoll === 4) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 4";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[3];
    } if (d20ComputerRoll === 5) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 5";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[4];
    } if (d20ComputerRoll === 6) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 6";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[5];
    } if (d20ComputerRoll === 7) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 7";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[6];
    } if (d20ComputerRoll === 8) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 8";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[7];
    } if (d20ComputerRoll === 9) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 9";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[8];
    } if (d20ComputerRoll === 10) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 10";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[9];
    } if (d20ComputerRoll === 11) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 11";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[10];
    } if (d20ComputerRoll === 12) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 12";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[11];
    } if (d20ComputerRoll === 13) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 13";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[12];
    } if (d20ComputerRoll === 14) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 14";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[13];
    } if (d20ComputerRoll === 15) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 15";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[14];
    } if (d20ComputerRoll === 16) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 16";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[15];
    } if (d20ComputerRoll === 17) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 17";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[16];
    } if (d20ComputerRoll === 18) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 18";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[17];
    } if (d20ComputerRoll === 19) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 19";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[18];
    }if (d20ComputerRoll === 20) {
+    document.getElementById('d20-dice-image-two').alt = "you rolled a 20";
     document.getElementById('d20-dice-image-two').src = d20DiceImageArray[19];
    }
   
